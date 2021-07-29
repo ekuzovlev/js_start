fe0bbe55bb17d44df9367833c4dba11cbe0334f1
kuz-en <bio19@ya.ru>
2021-07-29 18:08:16 +0300

    add variables

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+let money = 1000;
+
+let income = 200; 
+
+let addExpenses = 100; 
+
+let deposit = 300;
+
+let mission = 'books';
+
+let period = '29.07.2021';
+
+alert('Hello World!');
+
+console.log('Hello World!!!');
