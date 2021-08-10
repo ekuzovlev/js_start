08ac81e141d9a448d37621840799efb102273a9d
kuz-en <bio19@ya.ru>
2021-08-10 16:55:49 +0300

    forgot to save ;-)

--- js/script.js
+++ js/script.js
@@ -150,12 +150,12 @@ let depositCheckmark = document.querySelector('#deposit-check');
 let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
 
 // Каждый элемент в правой части программы через класс(не через querySelector), которые имеют в имени класса "-value", начиная с class="budget_day-value" и заканчивая class="target_month-value">
-let budgetDayValue = document.getElementsByClassName('budget_day-value');
-let expensesMonthValue = document.getElementsByClassName('expenses_month-value');
-let additionalIncomeValue = document.getElementsByClassName('additional_income-value');
-let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
-let incomePeriodValue = document.getElementsByClassName('income_period-value');
-let targetMonthValue = document.getElementsByClassName('target_month-value');
+let budgetDayValue = document.getElementsByClassName('budget_day-value')[0];
+let expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
+let additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0];
+let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0];
+let incomePeriodValue = document.getElementsByClassName('income_period-value')[0];
+let targetMonthValue = document.getElementsByClassName('target_month-value')[0];
 
 // Оставшиеся поля через querySelector каждый в отдельную переменную:
 let budgetMonthValue = document.querySelector('.budget_month-value');
